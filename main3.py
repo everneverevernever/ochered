1d8f5419ec84ecc5c92bd48b954daef662329b48
gleb_z <118896591+everneverevernever@users.noreply.github.com>
2023-06-08 22:41:14 +0300

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from sqlite3 import *
 from tkinter import ttk
+import sqlite3
 
 def ochered_view():
     def get_data_from_database():
@@ -23,14 +23,14 @@ def ochered_view():
         number1_l.config(text=new_text)
 
 
-    root = Tk()
-    root.title('Отображение очереди')
-    root.minsize(800, 450)
+    ochered_window = Tk()
+    ochered_window.title('Отображение очереди')
+    ochered_window.minsize(800, 450)
 
-    frame_number = Frame(root, width=150, height=150, bg='white')
+    frame_number = Frame(ochered_window, width=150, height=150, bg='white')
     frame_number.place(x=0, y=0)
 
-    frame_window = Frame(root, width=150, height=150, bg='white')
+    frame_window = Frame(ochered_window, width=150, height=150, bg='white')
     frame_window.place(x=400, y=0)
 
     number_in_ochered = ttk.Label(frame_number, text="Номер в очереди")
@@ -63,7 +63,66 @@ def ochered_view():
     window3_l.grid(row=4, column=0, sticky='w', padx=10, pady=10)
     window4_l.grid(row=5, column=0, sticky='w', padx=10, pady=10)
 
-    root.mainloop()
+def ochered_zapis():
+    def add_to_queue():
+        global next_number
+
+        queue.append(next_number)
+        queue_number_label.config(text=next_number)
+        next_number += 1
+
+        conn = sqlite3.connect('mydatabase.db')
+
+        # Добавление данных
+        cursor = conn.cursor()
+        entry_name_get = queue_name.get()
+        insert_inf = (entry_name_get,)
+        query = (""" INSERT INTO mytable(name) VALUES (?)""")
+
+        cursor.execute(query, insert_inf)
+
+        # Сохранение изменений
+        conn.commit()
+
+    window_zapis = Tk()
+    window_zapis.title("Очередь с регистрацией")
+
+    queue = []
+    next_number = 1
+
+    # добавим элементы интерфейса
+    label = Label(window_zapis, text="Вы стоите в очереди под номером:")
+    label.pack()
+
+    queue_number_label = Label(window_zapis, text="")
+    queue_number_label.pack()
+
+    queue_name = Entry(window_zapis)
+    queue_name.pack()
+
+    button = Button(window_zapis, text="Встать в очередь")
+    button.pack()
+    button.config(command=add_to_queue)
 
 
-ochered_view()
+# Создаем главное окно приложения
+root = Tk()
+root.geometry('450x400')
+root.title('Моя программа')
+
+# Создаем два фрейма
+left_frame = Frame(root, bd=2, relief='groove')
+left_frame.place(x=20, y=20, width=400, height=200)
+
+# Создаем кнопки на левом фрейме
+button1 = ttk.Button(left_frame, text='Просмотр очереди', command=ochered_view)
+button1.pack(pady=20)
+button2 = ttk.Button(left_frame, text='Запись в очередь', command=ochered_zapis)
+button2.pack(pady=20)
+
+# Создаем кнопку выход на нижнем фрейме
+button3 = ttk.Button(left_frame, text='Выход', command=root.destroy)
+button3.pack(pady=20)
+
+# Запускаем главный цикл
+root.mainloop()
